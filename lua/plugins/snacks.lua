21680a617124cad8aba2925bac465e1e1e93a8af
f4lc09 <mr.sokol5253@gmail.com>
2025-12-17 17:07:31 +0300

    Cool

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -2,6 +2,36 @@ return {
   "folke/snacks.nvim",
   opts = {
     picker = {
+      formatters = {
+        file = {
+          filename_first = true,
+        },
+      },
+      actions = {
+        cd_to_folder = function(picker)
+          local item = picker:current()
+          if not item then
+            return
+          end
+
+          local path = item.file
+          if vim.fn.isdirectory(path) == 0 then
+            path = vim.fn.fnamemodify(path, ":p:h")
+          end
+
+          picker:close()
+
+          for _, bufnr in ipairs(vim.api.nvim_list_bufs()) do
+            if vim.api.nvim_buf_is_loaded(bufnr) and vim.bo[bufnr].buftype == "" then
+              vim.api.nvim_buf_delete(bufnr, { force = true })
+            end
+          end
+
+          vim.api.nvim_set_current_dir(path)
+
+          print("CWD changed to: " .. path .. " (Buffers cleared)")
+        end,
+      },
       sources = {
         grep = {
           hidden = true,
@@ -37,6 +67,11 @@ return {
         },
       },
       win = {
+        list = {
+          keys = {
+            ["<C-d>"] = { "cd_to_folder", mode = { "n", "i" } },
+          },
+        },
         input = {
           keys = {
             ["<C-o>"] = {
@@ -44,7 +79,7 @@ return {
                 vim.cmd("wincmd p")
               end,
               mode = { "n", "i" },
-              desc = "Focus file tree with Ctrl+Shift+Enter",
+              desc = "Focus file tree with",
             },
           },
         },
