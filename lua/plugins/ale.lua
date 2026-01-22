9995f96fadd4693f07c209222360471c668096ec
f4lc09 <mr.sokol5253@gmail.com>
2026-01-22 12:56:39 +0300

    Fix ale

--- lua/plugins/ale.lua
+++ lua/plugins/ale.lua
@@ -7,6 +7,7 @@ return {
     -- This example sets ALE to lint when you leave insert mode or write the buffer
     vim.g.ale_lint_on_insert_leave = 1
     vim.g.ale_lint_on_buf_write = 1
+    vim.g.ale_echo_cursor = 0
     -- Set the specific linters for the 'go' filetype
     vim.g.ale_linters = {
       go = { "golangci-lint" },
