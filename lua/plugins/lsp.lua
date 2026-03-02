96050a75c5cf6c1cd443c44441d28c486e4528b9
f4lc09 <mr.sokol5253@gmail.com>
2026-03-02 15:30:23 +0300

    fix errors in diff view

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -6,7 +6,19 @@ return {
     "neovim/nvim-lspconfig",
     opts = {
       servers = {
+        on_new_config = function(new_config, new_root_dir)
+          -- Если URI не начинается с file://, отменяем запуск gopls для этого буфера
+          if not new_config.cmd_env then
+            new_config.cmd_env = {}
+          end
+        end,
         gopls = {
+          on_attach = function(client, bufnr)
+            local uri = vim.uri_from_bufnr(bufnr)
+            if uri:sub(1, 4) ~= "file" then
+              vim.lsp.buf_detach_client(bufnr, client.id)
+            end
+          end,
           settings = {
             gopls = {
               usePlaceholders = false,
