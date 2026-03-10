199070bfb9cd8763ed8526b36e2f3b448c5f2448
f4lc09 <mr.sokol5253@gmail.com>
2026-03-10 15:21:06 +0300

    Fix bufferline

---
+++ lua/plugins/befferline.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    "akinsho/bufferline.nvim",
+    opts = {
+      options = {
+        -- Всегда показывать панель буферов
+        always_show_bufferline = true,
+      },
+    },
+  },
+}
