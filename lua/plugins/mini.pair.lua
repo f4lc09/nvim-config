17f99e1e7952734c118d1c7235f84fe519f4d01b
f4lc09 <mr.sokol5253@gmail.com>
2026-05-15 19:09:09 +0300

    Fix minipairs

--- lua/plugins/mini.pair.lua
+++ lua/plugins/mini.pair.lua
@@ -3,5 +3,32 @@ return {
   enabled = true,
   opts = {
     modes = { command = false },
+    mappings = {
+      ["("] = { action = "open", pair = "()", neigh_pattern = "^[^\\]" },
+      ["["] = { action = "open", pair = "[]", neigh_pattern = "^[^\\]" },
+      ["{"] = { action = "open", pair = "{}", neigh_pattern = "^[^\\]" },
+
+      [")"] = { action = "close", pair = "()", neigh_pattern = "^[^\\]" },
+      ["]"] = { action = "close", pair = "[]", neigh_pattern = "^[^\\]" },
+      ["}"] = { action = "close", pair = "{}", neigh_pattern = "^[^\\]" },
+      ["'"] = {
+        action = "closeopen",
+        pair = "''",
+        neigh_pattern = "^[^%w\\]",
+        register = { cr = false },
+      },
+      ['"'] = {
+        action = "closeopen",
+        pair = '""',
+        neigh_pattern = "^[^%w\\]",
+        register = { cr = false },
+      },
+      ["`"] = {
+        action = "closeopen",
+        pair = "``",
+        neigh_pattern = "^[^%w\\]",
+        register = { cr = false },
+      },
+    },
   },
 }
