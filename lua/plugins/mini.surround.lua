c45d20f3f56eb22c1092ab8f8a6e5d810563922d
f4lc09 <mr.sokol5253@gmail.com>
2026-05-15 19:22:06 +0300

    Ura

---
+++ lua/plugins/mini.surround.lua
@@ -0,0 +1,15 @@
+return {
+  "mini.nvim/mini.surround",
+  version = false,
+  opts = {
+    mappings = {
+      add = "",
+      delete = "",
+      replace = "",
+      find = "",
+      find_left = "",
+      highlight = "",
+      update_n_lines = "",
+    },
+  },
+}
