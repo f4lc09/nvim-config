4e66563ac8d279ba6cf409f0098ab41ee0fe720a
f4lc09 <mr.sokol5253@gmail.com>
2026-06-09 17:33:22 +0300

    Better grug

--- lua/plugins/grug.lua
+++ lua/plugins/grug.lua
@@ -5,13 +5,18 @@ return {
       {
         "<leader>sr",
         function()
+          local buf = vim.api.nvim_create_buf(false, true)
+
           require("grug-far").open({
-            transient = true,
+            windowCreationCommand = "buffer " .. buf,
+            transient = false,
             prefills = {
               filesFilter = nil,
               flags = "-F --multiline",
             },
           })
+          vim.bo.buflisted = true
+          vim.api.nvim_buf_set_name(0, "Grug-Far (Search)")
         end,
         mode = { "n", "x" },
         desc = "Search and Replace (No Extension Filter)",
