7e07ebe3ff1aad8523428c1fccb15d0ef2457047
f4lc09 <mr.sokol5253@gmail.com>
2026-06-11 18:07:47 +0300

    fx

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -49,7 +49,6 @@ map({ "n", "t", "i" }, "<M-t>", utils.ToggleTmuxTerminal, { desc = "Toggle Termi
 map({ "n", "t", "i" }, "<C-_>", utils.ToggleTerminal, { desc = "Toggle Terminal" })
 map({ "t" }, "<C-n>", [[<C-\><C-n>]], { desc = "Go normanl mode from terminal" })
 map({ "t" }, "<C-u>", [[<C-\><C-n><C-u>]], { desc = "Go normanl mode from terminal" })
-map("t", "<C-c><C-c>", [[<C-\><C-n>:bd!<CR>]], { desc = "Fast kill terminal" })
 
 --
 -- Paste Commands
